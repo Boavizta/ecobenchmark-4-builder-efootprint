302b11c8e49a86b758640ad1cad93d9f7a78545e
Youen Chéné <youen.chene@gadz.org>
2024-05-24 17:04:39 +0200

    feat: add init runner

--- script/run-all.sh
+++ script/run-all.sh
@@ -26,13 +26,30 @@ function run() {
 }
 
 
+function run_init() {
+  run_id=$(date +%s)
+  extra_vars="server_application_host=$SERVER_APPLICATION_HOST"
+  extra_vars="$extra_vars server_application_user=$SERVER_APPLICATION_USER"
+  extra_vars="$extra_vars server_database_host=$SERVER_DATABASE_HOST"
+  extra_vars="$extra_vars server_database_user=$SERVER_DATABASE_USER"
+  extra_vars="$extra_vars server_runner_host=$SERVER_RUNNER_HOST"
+  extra_vars="$extra_vars server_runner_user=$SERVER_RUNNER_USER"
+  ansible-playbook \
+    -i script/hosts \
+    -i script/inventory-$1 \
+    script/site.yml \
+    --skip-tags install \
+    --extra-vars "$extra_vars service=$1 scenario=$2 output_directory=$PWD/results run_id=$run_id"
+}
+
+
 function run_all_services() {
   folder_path=../service
   folder_names=$(find $folder_path -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
   for folder_name in $folder_names; do
     if [[ $folder_name != *mysql ]]; then #temp patch
       #eval "run $folder_name"
-      run $folder_name INIT
+      run_init $folder_name INIT
       run $folder_name DATA_WRITE
       run $folder_name DATA_LIST
       run $folder_name DATA_SIMPLE_ANALYTIC
