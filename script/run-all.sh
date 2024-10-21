c56c1f19e5971bebaf0ed429e8e58c5d45d64531
Youen Chéné <youen.chene@gadz.org>
2024-10-21 15:56:39 +0200

    chore: divide into 3 ansible set of roles.

--- script/run-all.sh
+++ script/run-all.sh
@@ -3,7 +3,7 @@
 set -xe
 
 # run <service> <use_case>
-function run_once() {
+function run_test() {
   run_id=$(date +%s)
   extra_vars="server_application_host=$SERVER_APPLICATION_HOST"
   extra_vars="$extra_vars server_application_user=$SERVER_APPLICATION_USER"
@@ -12,15 +12,36 @@ function run_once() {
   extra_vars="$extra_vars server_runner_host=$SERVER_RUNNER_HOST"
   extra_vars="$extra_vars server_runner_user=$SERVER_RUNNER_USER"
   ansible-playbook \
-    -i script/hosts \
-    -i script/inventory-$1 \
-    script/site.yml \
+    -u debian \
+    -i inventory.ini \
+    -i hosts \
+    -i inventory-$1 \
+    site-test-runner.yml \
+    --skip-tags install \
+    --extra-vars "$extra_vars service=$1 scenario=$2 output_directory=$PWD/results run_id=$run_id"
+}
+
+# run <service> <use_case>
+function run_init_env() {
+  run_id=$(date +%s)
+  extra_vars="server_application_host=$SERVER_APPLICATION_HOST"
+  extra_vars="$extra_vars server_application_user=$SERVER_APPLICATION_USER"
+  extra_vars="$extra_vars server_database_host=$SERVER_DATABASE_HOST"
+  extra_vars="$extra_vars server_database_user=$SERVER_DATABASE_USER"
+  extra_vars="$extra_vars server_runner_host=$SERVER_RUNNER_HOST"
+  extra_vars="$extra_vars server_runner_user=$SERVER_RUNNER_USER"
+  ansible-playbook \
+    -u debian \
+    -i inventory.ini \
+    -i hosts \
+    -i inventory-$1 \
+    site-init-env.yml \
     --skip-tags install \
     --extra-vars "$extra_vars service=$1 scenario=$2 output_directory=$PWD/results run_id=$run_id"
 }
 
 function run() {
-  for i in {1..10}; do
+  for i in {1..4}; do #was 10
     run_once $1 $2
   done
 }
@@ -35,9 +56,11 @@ function run_init() {
   extra_vars="$extra_vars server_runner_host=$SERVER_RUNNER_HOST"
   extra_vars="$extra_vars server_runner_user=$SERVER_RUNNER_USER"
   ansible-playbook \
-    -i script/hosts \
-    -i script/inventory-$1 \
-    script/site.yml \
+    -u debian \
+    -i inventory.ini \
+    -i hosts \
+    -i inventory-$1 \
+    site-init-runner.yml \
     --skip-tags install \
     --extra-vars "$extra_vars service=$1 scenario=$2 output_directory=$PWD/results run_id=$run_id"
 }
@@ -49,10 +72,11 @@ function run_all_services() {
   for folder_name in $folder_names; do
     if [[ $folder_name != *mysql ]]; then #temp patch
       #eval "run $folder_name"
-      run_init $folder_name INIT
-      run $folder_name DATA_WRITE
-      run $folder_name DATA_LIST
-      run $folder_name DATA_SIMPLE_ANALYTIC
+      #run_init_env $folder_name #deploy architecture
+      run_init $folder_name INIT #Init account
+      run_test $folder_name DATA_WRITE #Run test 1
+      run_test $folder_name DATA_LIST #Run test 1
+      run_test $folder_name DATA_SIMPLE_ANALYTIC #Run test 1
     fi
   done
 }
